51250b12652bf93f3305ec3066e776edfad1f1f4
Divine <divineabraham007@gmail.com>
2018-09-21 00:14:58 +0100

    added more notes

--- Resources/src/main/resources/testing.notes.java
+++ Resources/src/main/resources/testing.notes.java
@@ -1,15 +1,19 @@
+// Test Suite
 @RunWith(Suite.class)
 @SuiteClasses({ DDTReadOnly.class, OpenReport.class })
 public class AllTests {
 
 }
 
+// Cucumber Test
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "TeaTesting.feature")
 public class testRunner
 {	
 	static ExtentReports report = new ExtentReports("report.html",true);	
 }
+
+// Dependenices
 	<dependencies>
   	<dependency>
   		<groupId>junit</groupId>
@@ -42,7 +46,7 @@ public class testRunner
   		<version>1.2.5</version>
   	</dependency>
   </dependencies>
-  
+// Default setup  
 	WebDriver driver = null;
 	static ExtentReports report;
 	ExtentTest test;
@@ -69,5 +73,53 @@ public class testRunner
 		report.flush();
 	}
 
-Wait for WebElement (not done) and Screenshot (done)
-Write to Excel File
+	// Asserts
+	assertTrue(“Boolean was False”, Boolean)
+	assertFalse(“Boolean was True”, Boolean)
+	assertEquals(“Actual was not Expected”, Expected, Actual)
+	assertEquals(“Actual was not expected within the Tolerance”, Expected, Actual, Tolerance)
+	fail(“Method Failed the Test”)
+	assertNull(“The object is not Null”, Object)
+	assertNotNull(“The object is Null”, Object)
+	assertSame(“The objects are not the same”, Expected, Actual)
+	assertNotSame(“The objects not the same”, Expected, Actual)
+	
+	// Drivers
+	(Chrome example)
+	System.setProperty("webdriver.chrome.driver","C:\\filepath\\chromedriver.exe");
+	(FireFox example)
+	System.setProperty("webdriver.gecko.driver", "C:\\filepath\\geckodriver.exe");
+	
+	// Find WebElement
+	By.name(String)
+	By.id(String)
+	By.className(String)
+	By.linkText(String
+	By.XPath(String)
+	By.CSSSelector(String)
+	By.partialLinkText(String)
+	By.tagName
+	
+	// WebElement
+	Webdriver.get()
+	Webdriver.findElement(By.)
+	Webdriver.findElements(By.)
+	.click()
+	.sendKeys()
+	isDisplayed()
+	.getAttribute()
+	.submit()
+	
+	.sendKeys(Keys.chord(Keys.SHIFT, "qa consulting")); // capitalise keys, simulates user...
+	
+	// Actions
+	DoubleClick
+	ContextClick
+	keyDown(Keys key)
+	keyUp(Keys key)
+	sendKeys(CharSequence keys)
+	
+	// Wait
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); (dont use, implicit wait)
+	WebElement myDynamicElement = (new WebDriverWait(driver, 10)) .until(ExpectedConditions.presenceOfElementLocated(By.id("elementID"))); 
+
